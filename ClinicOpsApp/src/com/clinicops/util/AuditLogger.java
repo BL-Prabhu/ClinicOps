861a610474ae127a093757cd7a1e19626e8153c3
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-30 10:36:00 +0530

    [Prabhu] Added : UC14 - Audit Logger

--- ClinicOpsApp/src/com/clinicops/util/AuditLogger.java
+++ ClinicOpsApp/src/com/clinicops/util/AuditLogger.java
@@ -7,6 +7,11 @@ import java.util.List;
 
 public class AuditLogger {
 
+    public static final String INFO = "INFO";
+    public static final String WARNING = "WARNING";
+    public static final String ERROR = "ERROR";
+    public static final String SECURITY = "SECURITY";
+
     private static final List<String> auditLogs = new ArrayList<>();
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
